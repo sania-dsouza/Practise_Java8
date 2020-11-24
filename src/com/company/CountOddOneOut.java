21a94e1cf0a29e1157e1fc220e677f6e6ed482e1
Sania <sania.d@myplanet.com>
2020-11-24 14:31:24 -0500

    Change to the odd one out program

--- src/com/company/CountOddOneOut.java
+++ src/com/company/CountOddOneOut.java
@@ -1,17 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class CountOddOneOut {
     public static void main(String[] args) {
-        int arr[] = {1,2,31,1,1,1,2,2,3,3,5};
+        System.out.println("Enter length of the array to be entered:");
+        Scanner scan = new Scanner(System.in);
+        int arrLen = scan.nextInt();
+        int arr[] = new int[arrLen];
+        System.out.println("Enter some numbers for an array");
+
+        for(int i = 0;i<arrLen; i++) {
+            arr[i] = scan.nextInt();
+        }
 
         for (int i = 0; i<arr.length; i++){
             int count = 0;
-            for(int j = 0; j<arr.length; j++){
+            for(int j = 0; j<arr.length; j++)
                 if (arr[i] == arr[j]) count++;
-            if (count % 2 != 0){
-                System.out.format("%d", arr[i]);
-            }
-            }
+            if (count % 2 != 0)
+                System.out.format("The odd number in the array is %d", arr[i]);
         }
     }
 }
